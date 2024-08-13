2f1c1b5acb769ab1adddf332ddd34fa113afce7f
Lecrut <filip.lecrut12@gmail.com>
2024-08-13 21:37:59 +0200

    add vuetify

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-main>
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </NuxtLayout>
 </template>
---
+++ plugins/vuetify.ts
@@ -0,0 +1,50 @@
+// import this after install `@mdi/font` package
+import '@mdi/font/css/materialdesignicons.css'
+
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+export default defineNuxtPlugin((app) => {
+    const vuetify = createVuetify({
+        components: {
+
+        },
+        theme: {
+            defaultTheme: 'dark',
+            themes: {
+                light: {
+                    dark: false,
+                    colors: {
+                        primary: '#0054db',
+                        error: '#FF0110',
+                    },
+                },
+                dark: {
+                    dark: true,
+                    colors: {
+                        primary: '#0054db',
+                        error: '#FF0110',
+                    },
+                },
+            },
+        },
+        defaults: {
+            VBtn: {
+                rounded: 'm',
+                color: 'primary',
+                variant: 'elevated',
+            },
+            VTextField: {
+                variant: 'underlined',
+            },
+            VCombobox: {
+                variant: 'underlined',
+            },
+            VSelect: {
+                variant: 'underlined',
+            }
+        },
+    })
+    app.vueApp.use(vuetify)
+})
