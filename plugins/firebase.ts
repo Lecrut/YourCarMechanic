7ec6a93752fc835f434b548ec75c71bc68311696
Lecrut <filip.lecrut12@gmail.com>
2024-08-14 22:41:31 +0200

    add firebase 3

---
+++ plugins/firebase.ts
@@ -0,0 +1,34 @@
+import type { FirebaseOptions } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig()
+
+    const firebaseConfig = {
+        apiKey: config.public.apiKey,
+        authDomain: config.public.authDomain,
+        projectId: config.public.projectId,
+        storageBucket: config.public.storageBucket,
+        messagingSenderId: config.public.messagingSenderId,
+        appId: config.public.appId,
+        measurementId: config.public.measurementId,
+    } as FirebaseOptions
+
+    const app = initializeApp(firebaseConfig, 'primary')
+
+    const auth = getAuth(app)
+    const firestore = getFirestore(app)
+    const storage = getStorage(app)
+
+    nuxtApp.vueApp.provide('auth', auth)
+    nuxtApp.provide('auth', auth)
+
+    nuxtApp.vueApp.provide('firestore', firestore)
+    nuxtApp.provide('firestore', firestore)
+
+    nuxtApp.vueApp.provide('storage', storage)
+    nuxtApp.provide('storage', storage)
+})
