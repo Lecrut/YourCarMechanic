aad7cd04f2169d655a98e0b35ad87b3cbd2af876
Lecrut <filip.lecrut12@gmail.com>
2024-08-18 09:21:55 +0200

    create reset password page

--- pages/auth/reset-password.vue
+++ pages/auth/reset-password.vue
@@ -1,10 +1,52 @@
 <script setup lang="ts">
-
+  const { t } = useI18n()
 </script>
 
 <template>
+  <v-sheet
+      class="d-flex align-center justify-center flex-wrap text-center mx-auto my-10 px-4"
+      elevation="4"
+      max-width="1100"
+      rounded
+  >
+    <v-row justify="center">
+      <v-col cols="12" sm="12" md="6">
+        <div class="d-flex flex-column align-center justify-center h-100 mx-2 pa-6">
+          <v-img
+              class="mx-auto my-5 elevation-5"
+              rounded="xl"
+              :width="300"
+              aspect-ratio="4/3"
+              cover
+              src="/reset.jpg"
+          />
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" md="6">
+        <div class="d-flex flex-column align-center justify-center h-100 mx-2 py-4">
+          <div class="text-h5 my-2">
+            {{ t('resetPassword.title') }}
+          </div>
 
+          <p>{{ t('resetPassword.text') }}</p>
+
+          <form class="w-75 my-2">
+            <v-text-field
+                :label="t('resetPassword.email')"
+                placeholder="example@mail.com"
+                type="email"
+            />
+
+            <v-btn>
+              {{ t('resetPassword.button') }}
+            </v-btn>
+          </form>
+        </div>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
+
 
 <style scoped>
 
