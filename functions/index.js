8d5adb2e83f1287f3dbc04aa551f816e9bc3df34
Lecrut <filip.lecrut12@gmail.com>
2024-12-12 22:26:49 +0100

    small changes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const {onRequest} = require("firebase-functions/v2/https");
 var express = require('express');
+const cors = require("cors");
 
 const {addCar} = require("./handlers/cars/addCar");
 const {getUserCars} = require("./handlers/cars/getUserCars");
@@ -9,9 +10,18 @@ const {signUp} = require("./handlers/auth/signUp");
 const {logIn} = require("./handlers/auth/logIn");
 const {resetPassword} = require("./handlers/auth/sendResetPassword");
 
+const corsOptions = {
+    origin: 'https://yourcarmechanic-624a4.web.app'
+};
+
 var cars = express();
 
-cars.get("/", (req, res) => res.status(200).send("Hey there!"));
+cars.use(cors(corsOptions));
+cars.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+});
+
+cars.post("/", (req, res) => res.status(200).send("Hey there!"));
 cars.post('/add-car', addCar);
 cars.post('/get-user-cars', getUserCars);
 cars.post('/update-car', updateCar);
@@ -21,7 +31,12 @@ exports.cars = onRequest(cars);
 
 var auth = express();
 
-auth.get("/", (req, res) => res.status(200).send("Hey there!"));
+auth.use(cors(corsOptions));
+auth.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+});
+
+auth.post("/", (req, res) => res.status(200).send("Hey there!"));
 
 auth.post('/sign-up', signUp);
 auth.post('/log-in', logIn);
