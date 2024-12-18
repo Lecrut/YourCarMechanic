7d860a564927ecdc49ee8023bf601466e14fa9db
Lecrut <filip.lecrut12@gmail.com>
2024-12-18 21:44:14 +0100

    remember lang

--- app.vue
+++ app.vue
@@ -5,6 +5,7 @@ import {useLocalStorage} from "@vueuse/core";
 const {locale, setLocale} = useI18n()
 
 const theme = useTheme()
+const route = useRoute()
 
 const currentThemeStore = useLocalStorage('current-theme', 'dark')
 const currentLangStore = useLocalStorage('current-language', 'pl')
@@ -12,14 +13,30 @@ const currentLangStore = useLocalStorage('current-language', 'pl')
 onMounted(() => {
   if (currentLangStore.value) {
     setLocale(currentLangStore.value)
+    locale.value = currentLangStore.value
+  } else {
+    setLocale((navigator.languages.includes('pl') || navigator.languages.includes('pl-PL'))
+        ? locale.value = 'pl'
+        : locale.value = 'en')
   }
 
   if (currentThemeStore.value)
     theme.global.name.value = currentThemeStore.value
 })
 
+watch(route, () => {
+  if (currentLangStore.value) {
+    setLocale(currentLangStore.value)
+  } else {
+    setLocale((navigator.languages.includes('pl') || navigator.languages.includes('pl-PL'))
+        ? locale.value = 'pl'
+        : locale.value = 'en')
+  }
+}, {immediate: true})
+
 watch(currentLangStore, (newLang) => {
   setLocale(newLang)
+  locale.value = newLang
 })
 
 watch(currentThemeStore, (newTheme) => {
